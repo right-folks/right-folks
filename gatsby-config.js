38ceaeaf9c1f30c960afe125ed7bb1c275058970
Taras Omelianchuk <taras.omelianchuk@gmail.com>
2020-07-25 01:03:19 +0300

    Change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#EC407A`,
         theme_color: `#EC407A`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/icon.png` // This path is relative to the root of the site.
       }
     },
     {
