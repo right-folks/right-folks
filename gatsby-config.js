dcbe322c1d8fea5f12b2b7ed66aa49dbfae89da6
Taras Omelianchuk <taras.omelianchuk@gmail.com>
2020-07-25 00:28:11 +0300

    Enable PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,9 +75,9 @@ module.exports = {
         ],
         display: "swap"
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`
   ]
 }
