ca1e6d4fe31f104b58ab33ff6550dc2d86d34c8a
Taras Omelianchuk <taras.omelianchuk@gmail.com>
2020-07-30 01:18:32 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-aa0c6c4309bde06a75d7.js"
+    "url": "webpack-runtime-51769b30bca1427d9395.js"
   },
   {
     "url": "framework-76cfb7115782dc5b0ee0.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6ff9905a9cb1a4bc400de514f29d4383"
+    "revision": "c70a53cfdc51229a71bad0fd701218a5"
   },
   {
     "url": "static/webfonts/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_cJD3gnD_g.woff2"
